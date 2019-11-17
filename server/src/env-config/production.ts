2e7610aa2578ddfaad7c620082fec15903c80b11
Moses Godson <mosesgodson27@gmail.com>
2019-11-18 00:04:53 +0100

    make it better

--- server/src/env-config/production.ts
+++ server/src/env-config/production.ts
@@ -2,5 +2,6 @@ import { envConfig } from './development';
 
 envConfig.production = true;
 envConfig.env = 'production';
+envConfig.pamlight.projectKey = process.env.PROJECT_KEY;
 
 export const settings = envConfig;
