fc235b185f48fdfb5f11943805c4c657f50b534a
Moses Godson <mosesgodson27@gmail.com>
2019-11-18 00:10:06 +0100

    describe

--- server/dist/env-config/production.js
+++ server/dist/env-config/production.js
@@ -3,4 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const development_1 = require("./development");
 development_1.envConfig.production = true;
 development_1.envConfig.env = 'production';
+development_1.envConfig.pamlight.projectKey = process.env.PROJECT_KEY;
 exports.settings = development_1.envConfig;
--- server/src/env-config/index.ts
+++ server/src/env-config/index.ts
@@ -1,1 +1,1 @@
-export * from './development';
+export * from './production';
